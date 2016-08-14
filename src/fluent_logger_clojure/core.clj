6668b45ce370a198ff5f117110c39c201fee4d03
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-08-14 20:34:10 +0900

    add log function

--- src/fluent_logger_clojure/core.clj
+++ src/fluent_logger_clojure/core.clj
@@ -1,6 +1,22 @@
-(ns fluent-logger-clojure.core)
+(ns fluent-logger-clojure.core
+  (:import (org.fluentd.logger FluentLogger)
+           (java.util HashMap)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+
+(defn logger
+  ([tag-prefix]
+   (FluentLogger/getLogger tag-prefix))
+  ([tag host port]
+   (FluentLogger/getLogger tag host port)))
+
+(def ^{:dynamic true} *fluent-logger*)
+
+(defn log
+  ([tag key value timestamp] (log tag {key value} timestamp))
+  ([tag data timestamp]
+    (let [dic (new HashMap)]
+      (doseq [k (keys data)]
+        (.put dic (name k) (get data k)))
+      (.log *fluent-logger* tag dic timestamp))))
+

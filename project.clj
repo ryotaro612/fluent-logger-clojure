cb034813e14d6dd35dafe948a49122e2b1cfe4d7
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-08-16 08:14:01 +0900

    update to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fluent-logger-clojure "0.1.0-SNAPSHOT"
+(defproject fluent-logger-clojure "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License Version 2.0"

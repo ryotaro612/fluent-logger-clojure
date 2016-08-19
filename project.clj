1b22a3dad7654a20b7193fda9772409494fc4683
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-08-19 22:10:56 +0900

    add test

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject fluent-logger-clojure "0.1.0"
+(defproject fluent-logger-clojure "0.1.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License Version 2.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ;; https://mvnrepository.com/artifact/org.fluentd/fluent-logger
-                 [org.fluentd/fluent-logger "0.3.2"]])
+                 [org.fluentd/fluent-logger "0.3.2"]]
+  :aot [fluent-logger-clojure.core-test]
+  )
--- test/fluent_logger_clojure/core_test.clj
+++ test/fluent_logger_clojure/core_test.clj
@@ -2,7 +2,38 @@
   (:require [clojure.test :refer :all]
             [fluent-logger-clojure.core :refer :all]))
 
-(comment (deftest quickstart
-           (testing "quickstart"
-             (binding [*fluent-logger* (logger "tag-prefix")]
-               (log "tag" "k" "val")))))
+(gen-class
+  :name fluent_logger_clojure.core_test.Logger
+  :prefix "-"
+  :init init
+  :methods [[log [String java.util.Map Long] Boolean]]
+  :state state)
+
+
+(defn -init []
+  [[] (atom [])])
+
+(defn -log [this tag data timestamp]
+  (swap! (.state this) merge {:tag tag :data (zipmap (.keySet data) (.values data)) :timestamp timestamp})
+  true)
+
+(deftest quickstart
+  (testing "quickstart"
+    (let [logger (fluent_logger_clojure.core_test.Logger.)]
+      ;; log
+      (with-log logger
+                (log "tag" "key" "val")
+                (log "tag2" {:key1 1 :key2 "a"})
+                (log-ts "tag3" :a "v" 42)
+                (log-ts "tag3" {:2 "v"} 42))
+      ;; validate log
+      (let [results @(.state logger)
+            log (nth results 0)
+            log1 (nth results 1)
+            log2 (nth results 2)
+            log3 (nth results 3)]
+        (is (= '{:tag "tag", :data {"key" "val"}, :timestamp 0} log))
+        (is (= '{:tag "tag2", :data {"key1" 1, "key2" "a"}, :timestamp 0} log1))
+        (is (= '{:tag "tag3", :data {"a" "v"}, :timestamp 42} log2))
+        (is (= '{:tag "tag3", :data {"2" "v"}, :timestamp 42} log3))))))
+
